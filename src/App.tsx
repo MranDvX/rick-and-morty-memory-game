5d13708f4f1940128054f5fd4852d52ea3b5a5d3
Francisco Mancuello Vazquez <franmavazq@gmail.com>
2023-03-23 01:40:15 -0300

    feat: :sparkles: App structure routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { ApolloProvider } from '@apollo/client'
+import GraphQLClient from './services/rick-and-morty'
 
-function App () {
-  const [count, setCount] = useState(0)
+import Main from './components/Main/Main'
+import Game from './components/Game/Game'
+import './App.scss'
 
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => { setCount((count) => count + 1) }}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <ApolloProvider client={GraphQLClient}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/game" element={<Game />} />
+        </Routes>
+      </Router>
+    </ApolloProvider>
   )
 }
 
