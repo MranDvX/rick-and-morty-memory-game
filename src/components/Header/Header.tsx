6ac55ad367ddb0a719dde31fd039f672fb4eb240
Francisco Mancuello Vazquez <franmavazq@gmail.com>
2023-03-23 05:41:59 -0300

    refactor: :lipstick: BEM methodology

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,9 +4,9 @@ import './Header.scss'
 const Header: React.FC = () => {
   return (
     <header>
-      <div className="fm-header-container">
-        <img className='fm-header-logo' src={rickAndMortyLogo}/>
-        <span className='fm-header-span'>Juego de memoria</span>
+      <div className="fm-header">
+        <img className='fm-header__logo' src={rickAndMortyLogo}/>
+        <span className='fm-header__span'>Juego de memoria</span>
       </div>
     </header>
   )
