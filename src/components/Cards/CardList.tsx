02d971f0878e5b10f02ab350c521ddae227e2ccf
Francisco Mancuello Vazquez <franmavazq@gmail.com>
2023-03-25 20:16:03 -0300

    feat: :sparkles: add cards components (character cards, card list)

---
+++ src/components/Cards/CardList.tsx
@@ -0,0 +1,29 @@
+// CardList.tsx
+import React from 'react'
+import CharacterCard from '../Cards/CharacterCard'
+import './cardList.scss'
+
+interface CardListProps {
+  characters: any[]
+  handleCardClick: (id: string) => void
+}
+
+const CardList: React.FC<CardListProps> = ({ characters, handleCardClick }) => {
+  return (
+    <div className="game-board">
+      {characters.map((character: any) => (
+        <CharacterCard
+          key={character.id}
+          id={character.id}
+          name={character.name}
+          image={character.image}
+          flipped={character.flipped}
+          status={character.status}
+          onClick={() => { handleCardClick(character.id) }}
+        />
+      ))}
+    </div>
+  )
+}
+
+export default CardList
---
+++ src/components/Cards/CharacterCard.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './characterCard.scss'
+
+interface CharacterCardProps {
+  id: string
+  name: string
+  image: string
+  flipped: boolean
+  matched?: boolean
+  onClick: () => void
+}
+
+const CharacterCard: React.FC<CharacterCardProps> = ({
+  id,
+  name,
+  image,
+  flipped,
+  matched,
+  onClick
+}) => {
+  return (
+    <div
+      className={`character-card ${matched ? 'matched' : ''}`}
+      onClick={!flipped && !matched ? onClick : undefined}
+    >
+      {flipped || matched
+        ? (
+        <img src={image} alt={name} className="character-image" />
+          )
+        : (
+        <div className="character-back" />
+          )}
+    </div>
+  )
+}
+
+export default CharacterCard
